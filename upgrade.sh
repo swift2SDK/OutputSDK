36597067c10316bd0d70126316256f3657ec4416
ashen_23 <515783034@qq.com>
2020-03-18 12:52:33 +0800

    ""

--- upgrade.sh
+++ upgrade.sh
@@ -1,12 +1,30 @@
-echo $1
 
-git status
-git add .
-git commit -m "add"
-git pull
-git push
+# 提交 git
+commit() {
+    git status
+    git add .
+    git commit -m '"'$1'"'
+    git pull
+    git push
+}
 
 
-git tag -m "0.2.2" 0.2.2
-git push --tags
-pod trunk push OutputSDK.podspec
+
+if [ $# -eq 0 ]
+then
+    echo '\033[31m please input tag(eg: 1.2.4) \033[0m'
+
+else
+    commit
+    echo 'run next'
+fi
+
+
+
+
+
+
+
+# git tag -m "0.2.2" 0.2.2
+# git push --tags
+# pod trunk push OutputSDK.podspec
