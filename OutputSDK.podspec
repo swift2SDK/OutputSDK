63825c2bd686ece23fa73c4e907d97a40768bc53
ashen_23 <515783034@qq.com>
2020-03-18 18:28:30 +0800

    "1.3.9"

--- OutputSDK.podspec
+++ OutputSDK.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'OutputSDK'
-  s.version          = '1.3.8'
+  s.version          = '1.3.9'
   s.swift_versions   = '5'
   s.summary          = 'a published SDK.'
 
---
+++ SDK/OCDynamicSDK.framework/Headers/DyUtils.h
@@ -0,0 +1,18 @@
+//
+//  DyUtils.h
+//  OCDynamicSDK
+//
+//  Created by Shi Jian on 2020/3/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DyUtils : NSObject
+
++(void)dyRun;
+
+@end
+
+NS_ASSUME_NONNULL_END
